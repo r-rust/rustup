3b98bffba3473bcfeca5c7ea068014d1855c56e5
Jeroen Ooms <jeroenooms@gmail.com>
2018-05-05 20:11:23 +0200

    Update existing toolchains

--- R/rustup.R
+++ R/rustup.R
@@ -16,6 +16,9 @@ rustup_windows <- function(targets = c('i686-pc-windows-gnu', 'x86_64-pc-windows
     utils::download.file('https://win.rustup.rs/', destfile = init, mode = 'wb')    
     if(sys::exec_wait(init, c('-y', '--default-host', 'x86_64-pc-windows-gnu')))
       stop("Failed to run rustup-init")    
+  } else {
+    if(sys::exec_wait('rustup', c('update', 'stable')))
+      stop("Failed to run 'rustup update stable'")
   }
   info <- sys::exec_internal('rustup', 'show')
   for(x in targets){
